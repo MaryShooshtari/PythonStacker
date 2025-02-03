edf93e3acf9ac81cf9188be1d1172a0c41be648b
Cristina Giordano <cristina.giordano@cern.ch>
2025-02-03 17:23:36 +0100

    other mods to obtain pseudo-reweighing input shapes

--- pythonStacker/createBSMWeights.py
+++ pythonStacker/createBSMWeights.py
@@ -45,7 +45,7 @@ def get_bsmvariations_filename(storage: str, filename: str, eventclass, suffix="
 
 def reweight_and_write(reweighter, eventclass, tree, storage, filename, process):
     # should become a record rather than a single array, using the naming scheme I devised in the init
-    if filename in ["/pnfs/iihe/cms/store/user/cgiordan/AnalysisOutput/ReducedTuples/2024-09-09_09-40/Tree_TTTT_13TeV_LO_TopPhilicScalarSinglet_M0p4_C1p0e00_240611_092611_Run2SIM_UL2018NanoAOD_240612_213908_MCPrompt_2018_base.root", "/pnfs/iihe/cms/store/user/cgiordan/AnalysisOutput/ReducedTuples/2024-11-26_15-46/Tree_TTTT_13TeV_LO_TopPhilicScalarSinglet_M0p4_C1p0e00_240611_092611_Run2SIM_UL2018NanoAOD_240612_213908_MCPrompt_2018_base.root"]:
+    if filename in ["/pnfs/iihe/cms/store/user/cgiordan/AnalysisOutput/ReducedTuples/2024-09-09_09-40/Tree_TTTT_13TeV_LO_TopPhilicScalarSinglet_M0p4_C1p0e00_240611_092611_Run2SIM_UL2018NanoAOD_240612_213908_MCPrompt_2018_base.root", "/pnfs/iihe/cms/store/user/cgiordan/AnalysisOutput/ReducedTuples/2024-11-26_15-46/Tree_TTTT_13TeV_LO_TopPhilicScalarSinglet_M0p4_C1p0e00_240611_092611_Run2SIM_UL2018NanoAOD_240612_213908_MCPrompt_2018_base.root", "/pnfs/iihe/cms/store/user/cgiordan/AnalysisOutput/ReducedTuples/2025-01-19_09-11/Tree_TTTT_13TeV_LO_TopPhilicScalarSinglet_M0p4_C1p0e00_240611_092611_Run2SIM_UL2018NanoAOD_240612_213908_MCPrompt_2018_base.root"]:
         print("SS 0p4 2018, change the BSM variation reading")
         arrs = tree.arrays(["eftVariationsSel"], "eventClass==" + str(eventclass), aliases={"eftVariationsSel": "eftVariations[:, 1:3]"})
     else:
@@ -123,5 +123,5 @@ if __name__ == "__main__":
         print(f"running weights for class {args.eventclass}")
         reweight_and_write(reweighter, int(args.eventclass), tree, args.storage, args.inputfile, args.process)
     if args.pseudo:# and args.eventclass in ["3","4","5","6","8","9","10","11","12"]:
-            write_pseudo_nominal(int(args.eventclass), args.storage, args.inputfile, args.process)
+        write_pseudo_nominal(int(args.eventclass), args.storage, args.inputfile, args.process)
     print("Success!")
